fad173ab806d79cc0181082907940cc0685a074f
Ellis Trickett <ellis.trickett@gmail.com>
2020-10-01 23:08:14 +0100

    Chat useEffect joining socket emitting name and room

--- server/index.js
+++ server/index.js
@@ -11,7 +11,18 @@ const server = http.createServer(app);
 const io = socketio(server);
 
 io.on('connection', (socket) => {
-  console.log("we have a new connection")
+  console.log("we have a new connection");
+
+  socket.on('join', ({ name, room }, callback) => {
+    console.log(name, room);
+
+    const error = true;
+
+    if(error) {
+      callback({ error: 'error' })
+    }
+    
+  })
 
   socket.on('disconnect', () => {
     console.log('User had left!!!');
