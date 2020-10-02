988cf09e86007dd1a3ae57009a9d45d59c58484b
Ellis Trickett <ellis.trickett@gmail.com>
2020-10-02 14:18:27 +0100

    extracted input into seperate component

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client'
+import './Chat.css'
+import InfoBar from '../InfoBar/InfoBar'
+import Input from '../Input/Input'
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
   const ENDPOINT = 'localhost:4000'
 
   useEffect(() => {
@@ -27,8 +32,29 @@ const Chat = ({ location }) => {
     }
   }, [ENDPOINT, location.search]);
 
+  useEffect(() => {
+    socket.on('message', (message) => {
+      setMessages([...messages, message])
+    })
+  }, [messages])
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+
+    if(message) {
+      socket.emit('sendMessage', message, () => setMessage(''))
+    }
+  }
+
+  console.log(message, messages)
+
   return (
-    <h1>Chat</h1>
+    <div className="outer-container">
+      <div className="container">
+        <InfoBar room={room} />
+        <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+      </div>
+    </div>
   )
 }
 
